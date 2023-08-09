c4afa63cce2cfa872c8db630e27be5aaba4ed040
Giorgi Tsintsadze <giorgi.tsintsadze@scnadiweb.com>
2023-08-09 19:45:37 +0400

    Primitives

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 2.0;
+        double b = 3.0;
+
+        double result = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        System.out.println("Result: " + result);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        System.out.println("Integer representation of 'c': " + (int) c);
+        System.out.println("Integer representation of 'h': " + (int) h);
+        System.out.println("Integer representation of 'a': " + (int) a);
+        System.out.println("Integer representation of 'r': " + (int) r);
     }
 }
